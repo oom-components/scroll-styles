92926b19f5d307d2ca24c7f9f2143700a1e11704
Oscar Otero <oom@oscarotero.com>
2020-06-26 16:18:06 +0200

    removed node dependencies

--- demo/script.js
+++ demo/script.js
@@ -1,10 +1,10 @@
-import Scroll from '../src/scroll.js';
+import Scroll from "../src/scroll.js";
 
 const scroll = new Scroll();
 
-scroll.observe(document.querySelector('#element'), {
-    viewport: 0.5,
-    element: 0,
+scroll.observe(document.querySelector("#element"), {
+  viewport: 0.5,
+  element: 0,
 });
 
 scroll.init();
